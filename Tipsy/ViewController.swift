7e3e32f4fc5de136d1d98688c1f57faddb406596
Ranis <ranis25shar@gmail.com>
2023-03-20 18:29:25 +0300

    Initial Commit

---
+++ Tipsy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Ranis Sharifullin on 3/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
